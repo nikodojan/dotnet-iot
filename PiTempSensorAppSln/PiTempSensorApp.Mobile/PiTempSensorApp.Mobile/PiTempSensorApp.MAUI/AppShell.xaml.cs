6a4471b9e706a9efa1f983a257611079c6fb3b4f
Niko D <nikodojan@live.de>
2022-10-29 20:40:40 +0200

    maui app created

---
+++ PiTempSensorAppSln/PiTempSensorApp.Mobile/PiTempSensorApp.Mobile/PiTempSensorApp.MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PiTempSensorApp.MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PiTempSensorAppSln/PiTempSensorApp.Mobile/PiTempSensorApp.Mobile/PiTempSensorApp.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PiTempSensorApp.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
